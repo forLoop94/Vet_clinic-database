07a647587b01ee3acdcb4aca09a65fafaf0926eb
forLoop94 <charlesuwem94@gmail.com>
2023-07-13 09:12:37 +0100

    Add column species_id and owners_id to animals which are FOREIGN KEYS referencing owners and species tables

--- schema.sql
+++ schema.sql
@@ -25,3 +25,13 @@ CREATE TABLE species (
 	name VARCHAR(255),
 	PRIMARY KEY(id)
 );
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD species_id INTEGER;
+
+ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id);
+
+ALTER TABLE animals ADD owner_id INTEGER;
+
+ALTER TABLE animals ADD CONSTRAINT fk_owners FOREIGN KEY(owner_id) REFERENCES owners(id);
