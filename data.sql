55688db4db27799cb522cce6a7860385fc5ceb1a
forLoop94 <charlesuwem94@gmail.com>
2023-07-13 10:00:08 +0100

    Insert required data into species and owners tables

--- data.sql
+++ data.sql
@@ -155,3 +155,67 @@ INSERT INTO animals (
 	true,
 	22.0
 );
+
+-- Day-3
+
+/* DAY 3 */
+
+INSERT INTO owners (
+	full_name,
+	age
+) VALUES (
+	'Sam Smith',
+	34
+);
+
+INSERT INTO owners (
+	full_name,
+	age
+) VALUES (
+	'Jennifer Orwell',
+	19
+);
+
+INSERT INTO owners (
+	full_name,
+	age
+) VALUES (
+	'Bob',
+	45
+);
+
+INSERT INTO owners (
+	full_name,
+	age
+) VALUES (
+	'Melody Pond',
+	77
+);
+
+INSERT INTO owners (
+	full_name,
+	age
+) VALUES (
+	'Dean Winchester',
+	14
+);
+
+INSERT INTO owners (
+	full_name,
+	age
+) VALUES (
+	'Jodie Whittaker',
+	38
+);
+
+INSERT INTO species (
+	name
+) VALUES (
+	'Pokemon'
+);
+
+INSERT INTO species (
+	name
+) VALUES (
+	'Digimon'
+);
