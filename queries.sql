8c61e85144d050c36dad3c7b8387b89f38a20317
forLoop94 <charlesuwem94@gmail.com>
2023-07-13 11:39:01 +0100

    Write required queries and update queries.sql

--- queries.sql
+++ queries.sql
@@ -44,3 +44,13 @@ SELECT AVG(weight_kg) FROM animals;
 SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY (neutered);
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY (species);
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY (species);
+
+-- Day 3
+
+SELECT name, full_name FROM animals INNER JOIN owners ON owner_id = owners.id WHERE full_name = 'Melody Pond';
+SELECT animals.name AS animal_name, species.name AS species_name FROM animals INNER JOIN species ON animals.species_id = species.id WHERE species.name = 'Pokemon';
+SELECT full_name, name FROM animals RIGHT JOIN owners ON owners.id = animals.owner_id GROUP BY full_name, name;
+SELECT COUNT(*), species.name AS species_name FROM animals INNER JOIN species ON animals.species_id = species.id GROUP BY species.name;
+SELECT animals.name AS animal_name, species.name AS species_name, full_name FROM animals INNER JOIN species ON species.id = animals.species_id INNER JOIN owners ON owners.id = animals.owner_id WHERE species.name = 'Digimon' AND owners.full_name = 'Jennifer Orwell';
+SELECT * FROM animals INNER JOIN owners ON animals.owner_id = owners.id WHERE escape_attempts = 0 AND full_name = 'Dean Winchester';
+SELECT full_name, COUNT(animals.name) AS animals_owned FROM owners INNER JOIN animals ON owners.id = animals.owner_id GROUP BY full_name;
